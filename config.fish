3259280545e196f267c090d70643bb0740e16001
Heejong Ahn <heejongahn@gmail.com>
2016-06-29 19:52:34 +0200

    Add ~/.local/bin to PATH

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@
 status --is-interactive; and . (pyenv init -|psub)
 
 if type -q nvim; alias vim='nvim'; end # neovim
+set PATH $PATH ~/.local/bin
 
 # chips
 if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
