b9bc5160ce1e38997bc066ecd1b3b4a3e2129980
Heejong Ahn <heejongahn@gmail.com>
2016-11-20 20:33:32 +0900

    Add node_modules/bin to PATH

--- config.fish
+++ config.fish
@@ -4,7 +4,7 @@ status --is-interactive; and . (pyenv init -|psub)
 if type -q nvim; alias vim='nvim'; end # neovim
 alias ta='tmux attach'
 alias npm='yarn'
-set PATH $PATH ~/.local/bin
+set PATH $PATH ~/.local/bin node_modules/.bin/
 
 # chips
 if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
