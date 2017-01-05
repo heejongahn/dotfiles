ea0c13cbffb6830c0959d3509def9f8abe8ba0c2
Heejong Ahn <heejongahn@gmail.com>
2017-01-05 22:07:46 +0900

    Add pyenv & PostgreSQL.app

--- config.fish
+++ config.fish
@@ -1,10 +1,11 @@
-# pyenv
-status --is-interactive; and . (pyenv init -|psub)
-
 if type -q nvim; alias vim='nvim'; end # neovim
+
 alias ta='tmux attach'
-alias npm='yarn'
-set PATH $PATH ~/.local/bin node_modules/.bin/
+set PATH $PATH ~/.local/bin node_modules/.bin/ /Applications/Postgres.app/Contents/Versions/latest/bin
 
 # chips
 if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
+
+# pyenv & pyenv-virtualenv
+status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
