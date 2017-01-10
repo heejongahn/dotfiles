6b7d4334732328b02a346d1f4f6a580de1102a42
Heejong Ahn <heejongahn@gmail.com>
2017-01-10 13:04:20 +0900

    Use faster yet less convinient method of using pyenv&pyenv-virtualenv

--- config.fish
+++ config.fish
@@ -6,9 +6,43 @@ set PATH $PATH ~/.local/bin node_modules/.bin/ /Applications/Postgres.app/Conten
 # chips
 if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
 
-# pyenv & pyenv-virtualenv
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
-
+# pyenv &.python-version
 alias pev='pyenv'
 alias pevv='pyenv virtualenv'
+alias pea='pyenv_activate'
+alias ped='pyenv deactivate'
+
+function pyenv_init
+  setenv PATH '/Users/maru/.pyenv/shims' $PATH
+  setenv PYENV_SHELL fish
+  . '/usr/local/Cellar/pyenv/1.0.6/libexec/../completions/pyenv.fish'
+  command pyenv rehash 2>/dev/null
+  function pyenv
+    set command $argv[1]
+    set -e argv[1]
+
+    switch "$command"
+    case activate deactivate rehash shell
+      . (pyenv "sh-$command" $argv|psub)
+    case '*'
+      command pyenv "$command" $argv
+    end
+  end
+end
+
+function pyenv_virtualenv_init
+  setenv PATH '/usr/local/Cellar/pyenv-virtualenv/1.0.0/shims' $PATH;
+  setenv PYENV_VIRTUALENV_INIT 1;
+  if [ -n "$VIRTUAL_ENV" ]
+    pyenv activate --quiet; or pyenv deactivate --quiet; or true
+  else
+    pyenv activate --quiet; or true
+  end
+  return $status
+end
+
+function pyenv_activate
+  pyenv_init
+  pyenv_virtualenv_init
+  [ (cat .python-version) = (basename "$PYENV_VIRTUAL_ENV") ]
+end
