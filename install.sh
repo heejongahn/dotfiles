c899c6900311ead1524fe1864058e24a184bc825
Heejong Ahn <heejongahn@gmail.com>
2019-12-30 10:38:48 +0900

    install: Remove brew, youtube-dl, add python3

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
-# Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-
 # Utlities
-brew install zsh neovim node youtube-dl
+brew install zsh neovim node python3
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Make symlinks of dotfiles to home folder
@@ -20,7 +17,7 @@ ln -sf dotfiles/.gitconfig
 ln -sf dotfiles/.gitignore
 
 # Install vim-plug, plugins and Python provider
-pip install --user --upgrade neovim # For MatchTagAlways Plug
+pip3 install --user --upgrade neovim # For MatchTagAlways Plug
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall -c cq
